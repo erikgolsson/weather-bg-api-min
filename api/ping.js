40c92bcfc8235f831116bd4db43808d57f5de4d4
erikgolsson <hej@erikgolsson.se>
2025-12-11 20:53:12 +0100

    Update ping.js

--- api/ping.js
+++ api/ping.js
@@ -1,3 +1,10 @@
 export default function handler(req, res) {
+  // Tillåt anrop från din frontend-domän
+  res.setHeader("Access-Control-Allow-Origin", "https://erikgolsson.se");
+
+  // (valfritt) tillåt vissa headers om du vill
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  // Svara med enkel JSON
   res.status(200).json({ message: "pong" });
 }
